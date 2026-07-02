13855199e592db73c06ab904ff332ca4a76243fa
Andrew Gwinner <weblings.lings@gmail.com>
2026-07-01 22:40:58 -0700

    removed v1 and xrproto again since combined is working again

--- XRProto/src/XRTypes.ts
+++
@@ -1,9 +0,0 @@
-/** Tracks a DOM element for ray hit-test interaction in XR. */
-export interface XrButton {
-    el: HTMLElement;
-    onClick?: () => void;
-    // Drag-to-scrub: trigger-down starts, held frames move, trigger-up ends.
-    onScrubStart?: () => void;
-    onScrubMove?:  (normalizedX: number) => void;
-    onScrubEnd?:   (normalizedX: number) => void;
-}
